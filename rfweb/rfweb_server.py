797aa1952eb9405a7de38b53b7d2468a33a0c6b1
Allan Vidal <allanv@cpqd.com.br>
2012-05-24 14:54:59 -0300

    Show correct port number in rfweb_server.py.

--- rfweb/rfweb_server.py
+++ rfweb/rfweb_server.py
@@ -2,5 +2,5 @@ from wsgiref.simple_server import make_server
 import rfweb
 
 httpd = make_server("", 8080, rfweb.application)
-print "Serving on 0.0.0.0:8000"
+print "Serving on 0.0.0.0:8080"
 httpd.serve_forever()
