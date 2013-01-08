4e3fce87c0012cc288722a8438ddb9b1e16aa43d
Joe Stringer <joestringernz@gmail.com>
2013-01-08 15:33:27 +1300

    rfserver: Replace DatapathConfig with RouteMod
    This patch changes rfserver behaviour to no longer send DatapathConfig
    operations, but send equivalent RouteMods instead.
    
    Signed-off-by: Joe Stringer <joestringernz@gmail.com>

--- rfserver/rfserver.py
+++ rfserver/rfserver.py
@@ -170,10 +170,50 @@ class RFServer(RFProtocolFactory, IPC.IPCMessageProcessor):
                           (format_id(dp_id), dp_port, format(entry.vm_id), entry.vm_port))
 
     def send_datapath_config_message(self, ct_id, dp_id, operation_id):
-        self.ipc.send(RFSERVER_RFPROXY_CHANNEL, str(ct_id),
-                      DatapathConfig(ct_id=ct_id,
-                                     dp_id=dp_id,
-                                     operation_id=operation_id))
+        rm = RouteMod(RMT_ADD, dp_id)
+
+        if operation_id == DC_CLEAR_FLOW_TABLE:
+            rm.set_mod(RMT_DELETE)
+            rm.add_option(Option.PRIORITY(0))
+        elif operation_id == DC_DROP_ALL:
+            rm.add_option(Option.PRIORITY(1))
+            # No action specifies discard
+            pass
+        else:
+            if operation_id == DC_RIPV2:
+                rm.add_match(Match.ETHERTYPE(ETHERTYPE_IP))
+                rm.add_match(Match.NW_PROTO(IPPROTO_UDP))
+                rm.add_match(Match.IPV4(IPADDR_RIPv2, IPV4_MASK_EXACT))
+            elif operation_id == DC_OSPF:
+                rm.add_match(Match.ETHERTYPE(ETHERTYPE_IP))
+                rm.add_match(Match.NW_PROTO(IPPROTO_OSPF))
+            elif operation_id == DC_ARP:
+                rm.add_match(Match.ETHERTYPE(ETHERTYPE_ARP))
+            elif operation_id == DC_ICMP:
+                rm.add_match(Match.ETHERTYPE(ETHERTYPE_IP))
+                rm.add_match(Match.NW_PROTO(IPPROTO_ICMP))
+            elif operation_id == DC_BGP_PASSIVE:
+                rm.add_match(Match.ETHERTYPE(ETHERTYPE_IP))
+                rm.add_match(Match.NW_PROTO(IPPROTO_TCP))
+                rm.add_match(Match.TP_DST(TPORT_BGP))
+            elif operation_id == DC_BGP_ACTIVE:
+                rm.add_match(Match.ETHERTYPE(ETHERTYPE_IP))
+                rm.add_match(Match.NW_PROTO(IPPROTO_TCP))
+                rm.add_match(Match.TP_SRC(TPORT_BGP))
+            elif operation_id == DC_LDP_PASSIVE:
+                rm.add_match(Match.ETHERTYPE(ETHERTYPE_IP))
+                rm.add_match(Match.NW_PROTO(IPPROTO_TCP))
+                rm.add_match(Match.TP_DST(TPORT_LDP))
+            elif operation_id == DC_LDP_ACTIVE:
+                rm.add_match(Match.ETHERTYPE(ETHERTYPE_IP))
+                rm.add_match(Match.NW_PROTO(IPPROTO_TCP))
+                rm.add_match(Match.TP_SRC(TPORT_LDP))
+            elif operation_id == DC_VM_INFO:
+                rm.add_match(Match.ETHERTYPE(RF_ETH_PROTO))
+            rm.add_action(Action.CONTROLLER())
+
+        rm.add_option(Option.CT_ID(ct_id))
+        self.ipc.send(RFSERVER_RFPROXY_CHANNEL, str(ct_id), rm)
 
     def config_dp(self, ct_id, dp_id):
         if is_rfvs(dp_id) and dp_id not in self.configured_rfvs:
