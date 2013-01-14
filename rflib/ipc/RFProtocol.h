97703d53654e7c1b4f614f1695ebcec2d52cfc93
Joe Stringer <joestringernz@gmail.com>
2013-01-14 13:53:38 +1300

    rflib: Remove IPC RouteInfo,FlowMod messages
    Signed-off-by: Joe Stringer <joestringernz@gmail.com>

--- rflib/ipc/RFProtocol.h
+++ rflib/ipc/RFProtocol.h
@@ -15,8 +15,6 @@ enum {
 	PORT_REGISTER,
 	PORT_CONFIG,
 	DATAPATH_CONFIG,
-	ROUTE_INFO,
-	FLOW_MOD,
 	DATAPATH_PORT_REGISTER,
 	DATAPATH_DOWN,
 	VIRTUAL_PLANE_MAP,
@@ -93,96 +91,6 @@ class DatapathConfig : public IPCMessage {
         uint64_t ct_id;
         uint64_t dp_id;
         uint32_t operation_id;
-};
-
-class RouteInfo : public IPCMessage {
-    public:
-        RouteInfo();
-        RouteInfo(uint64_t vm_id, uint32_t vm_port, IPAddress address, IPAddress netmask, uint32_t dst_port, MACAddress src_hwaddress, MACAddress dst_hwaddress, bool is_removal);
-
-        uint64_t get_vm_id();
-        void set_vm_id(uint64_t vm_id);
-
-        uint32_t get_vm_port();
-        void set_vm_port(uint32_t vm_port);
-
-        IPAddress get_address();
-        void set_address(IPAddress address);
-
-        IPAddress get_netmask();
-        void set_netmask(IPAddress netmask);
-
-        uint32_t get_dst_port();
-        void set_dst_port(uint32_t dst_port);
-
-        MACAddress get_src_hwaddress();
-        void set_src_hwaddress(MACAddress src_hwaddress);
-
-        MACAddress get_dst_hwaddress();
-        void set_dst_hwaddress(MACAddress dst_hwaddress);
-
-        bool get_is_removal();
-        void set_is_removal(bool is_removal);
-
-        virtual int get_type();
-        virtual void from_BSON(const char* data);
-        virtual const char* to_BSON();
-        virtual string str();
-
-    private:
-        uint64_t vm_id;
-        uint32_t vm_port;
-        IPAddress address;
-        IPAddress netmask;
-        uint32_t dst_port;
-        MACAddress src_hwaddress;
-        MACAddress dst_hwaddress;
-        bool is_removal;
-};
-
-class FlowMod : public IPCMessage {
-    public:
-        FlowMod();
-        FlowMod(uint64_t ct_id, uint64_t dp_id, IPAddress address, IPAddress netmask, uint32_t dst_port, MACAddress src_hwaddress, MACAddress dst_hwaddress, bool is_removal);
-
-        uint64_t get_ct_id();
-        void set_ct_id(uint64_t ct_id);
-
-        uint64_t get_dp_id();
-        void set_dp_id(uint64_t dp_id);
-
-        IPAddress get_address();
-        void set_address(IPAddress address);
-
-        IPAddress get_netmask();
-        void set_netmask(IPAddress netmask);
-
-        uint32_t get_dst_port();
-        void set_dst_port(uint32_t dst_port);
-
-        MACAddress get_src_hwaddress();
-        void set_src_hwaddress(MACAddress src_hwaddress);
-
-        MACAddress get_dst_hwaddress();
-        void set_dst_hwaddress(MACAddress dst_hwaddress);
-
-        bool get_is_removal();
-        void set_is_removal(bool is_removal);
-
-        virtual int get_type();
-        virtual void from_BSON(const char* data);
-        virtual const char* to_BSON();
-        virtual string str();
-
-    private:
-        uint64_t ct_id;
-        uint64_t dp_id;
-        IPAddress address;
-        IPAddress netmask;
-        uint32_t dst_port;
-        MACAddress src_hwaddress;
-        MACAddress dst_hwaddress;
-        bool is_removal;
 };
 
 class DatapathPortRegister : public IPCMessage {
